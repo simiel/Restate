c4bc1cffde144c747e540f4d37f2772ba40bc72f
Simiel Adjei <smenxah@gmail.com>
2025-02-09 23:53:19 +0000

    db setup

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -13,10 +13,15 @@ import { openAuthSessionAsync } from "expo-web-browser";
 
 export const config = {
   platform: "com.simiel.restate",
-  // endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
-  endpoint: "https://cloud.appwrite.io/v1",
-  // projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
-  projectId: "67a76452001df269778e",
+  endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+  projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+  databaseId: process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID,
+  galleriesCollectionId:
+    process.env.EXPO_PUBLIC_APPWRITE_GALLERIES_COLLECTION_ID,
+  reviewsCollectionId: process.env.EXPO_PUBLIC_APPWRITE_REVIEWS_COLLECTION_ID,
+  agentsCollectionId: process.env.EXPO_PUBLIC_APPWRITE_AGENTS_COLLECTION_ID,
+  propertiesCollectionId:
+    process.env.EXPO_PUBLIC_APPWRITE_PROPERTIES_COLLECTION_ID,
 };
 
 export const client = new Client();
@@ -28,6 +33,7 @@ client
 
 export const avatar = new Avatars(client);
 export const account = new Account(client);
+export const databases = new Databases(client);
 
 export async function login() {
   try {
