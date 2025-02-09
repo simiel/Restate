a7bee58d7f8e28a561cc5e9037501088b0477252
Simiel Adjei <smenxah@gmail.com>
2025-02-09 21:13:49 +0000

    ...

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -35,9 +35,22 @@ const index = () => {
             className="size-6"
           />
         </View>
-      </View>
 
-      <Search />
+        <Search />
+
+        <View className="my-5">
+          <View className="flex flex-row items-center justify-between">
+            <Text className="text-xl font-rubik-bold text-black-300">
+              Featured
+            </Text>
+            <TouchableOpacity>
+              <Text className="text-base font-rubik-bold text-primary-300">
+                See All
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
     </SafeAreaView>
   );
 };
